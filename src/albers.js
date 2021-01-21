21cd08690ff0430ac089d9ab12ff342e99c1ac40
remy <remyhunter256@gmail.com>
2021-01-21 10:57:11 -0500

    Tidy up core code

--- src/albers.js
+++ src/albers.js
@@ -10,23 +10,16 @@ import quantize from 'quantize'
 
 export default function getColors(imgSrc, amtColors) {
 
-	var myPixels = [];
-
-
 	var c = document.createElement('canvas');   
 	var ctx = c.getContext("2d");
-	// document.body.appendChild(c);
 
-	var img = new Image;
+	var img = new Image();
 	img.src = imgSrc;
 	c.width = 120;
 	c.height = 120;
-	// c.style=
 	ctx.drawImage(img, 0, 0, c.width, c.height);
 	var imgData = ctx.getImageData(0, 0, c.width, c.height);
 
-	// the higher this resolution variable, the 
-	var resolution = 100;
 	for (var i = 0; i < imgData.data.length; i+= resolution * 4) {
 	  	let r=imgData.data[0 + i],
 		g=imgData.data[1 + i],
