f5b265c6d726e0eac9e0c92af7b0c0662c70362a
remy <remyhunter256@gmail.com>
2021-05-05 13:34:38 -0400

    cleanup

--- src/albers.js
+++ src/albers.js
@@ -12,28 +12,28 @@ var albers = (function() {
 
 	function getColors(imgSrc, amtColors){
 
-	var c = document.createElement('canvas');   
-	var ctx = c.getContext("2d");
+		var c = document.createElement('canvas');   
+		var ctx = c.getContext("2d");
 
-	var img = new Image();
-	img.src = imgSrc;
-	// c.width = 120;
-	// c.height = 120;
-	ctx.drawImage(img, 0, 0, c.width, c.height);
-	var imgData = ctx.getImageData(0, 0, c.width, c.height);
+		var img = new Image();
+		img.src = imgSrc;
+		c.width = 120;
+		c.height = 120;
+		ctx.drawImage(img, 0, 0, c.width, c.height);
+		var imgData = ctx.getImageData(0, 0, c.width, c.height);
 
-	for (var i = 0; i < imgData.data.length; i+= resolution * 4) {
-	  	let r=imgData.data[0 + i],
-		g=imgData.data[1 + i],
-		b=imgData.data[2 + i];
-		myPixels.push([r, g, b]);
-	}
+		for (var i = 0; i < imgData.data.length; i+= resolution * 4) {
+			let r=imgData.data[0 + i],
+			g=imgData.data[1 + i],
+			b=imgData.data[2 + i];
+			myPixels.push([r, g, b]);
+		}
 
-	var amtColors = amtColors;
-	var cmap = quantize(myPixels, amtColors);
-	var newPalette = cmap.palette();
+		var amtColors = amtColors;
+		var cmap = quantize(myPixels, amtColors);
+		var newPalette = cmap.palette();
 
-	return newPalette 
+		return newPalette 
 	}
 
 	return {
@@ -42,3 +42,4 @@ var albers = (function() {
 })();
 
 module.exports = albers.getColors;
+
